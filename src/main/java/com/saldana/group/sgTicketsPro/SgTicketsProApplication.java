b84afff7533f290b2d5c02baf8f88ac06a307a0c
NoelPerez <noel-93@hotmail.es>
2024-10-26 01:57:10 -0400

    agregando proyecto inicial

---
+++ src/main/java/com/saldana/group/sgTicketsPro/SgTicketsProApplication.java
@@ -0,0 +1,13 @@
+package com.saldana.group.sgTicketsPro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgTicketsProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgTicketsProApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/saldana/group/sgTicketsPro/SgTicketsProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saldana.group.sgTicketsPro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgTicketsProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
